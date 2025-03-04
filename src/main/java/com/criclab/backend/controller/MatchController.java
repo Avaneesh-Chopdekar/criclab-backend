ef347e82ce583e534e66ad41893e089a77ba4b62
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-05 00:28:25 +0530

    added cors support to localhost:4200 angular frontend

--- src/main/java/com/criclab/backend/controller/MatchController.java
+++ src/main/java/com/criclab/backend/controller/MatchController.java
@@ -4,6 +4,7 @@ import com.criclab.backend.entity.Match;
 import com.criclab.backend.services.MatchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/matches")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MatchController {
 
     private final MatchService matchService;
