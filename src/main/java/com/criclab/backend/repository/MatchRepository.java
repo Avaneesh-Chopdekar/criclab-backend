689cc6020fefcb37eaf28ebf0aeb847b7fe820cd
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-04 12:11:19 +0530

    added Repository annotation on MatchRepository interface

--- src/main/java/com/criclab/backend/repository/MatchRepository.java
+++ src/main/java/com/criclab/backend/repository/MatchRepository.java
@@ -2,9 +2,11 @@ package com.criclab.backend.repository;
 
 import com.criclab.backend.entity.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
 
     Optional<Match> findByTeamHeading(String teamHeading);
