4e39406113be13ae75c06de47f36c3b94065b4a6
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-04 21:14:41 +0530

    added implementation for MatchService interface

--- src/main/java/com/criclab/backend/services/impl/MatchServiceImpl.java
+++ src/main/java/com/criclab/backend/services/impl/MatchServiceImpl.java
@@ -6,8 +6,13 @@ import com.criclab.backend.services.MatchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
@@ -25,9 +30,63 @@ public class MatchServiceImpl implements MatchService {
         return matchRepository.findAll();
     }
 
+    /**
+     * Gets all live matches
+     *
+     * @return A list of all live matches.
+     */
     @Override
     public List<Match> getLiveMatches() {
-        return List.of();
+        List<Match> matches = new ArrayList<>();
+        try {
+            String url = "https://www.cricbuzz.com/cricket-match/live-scores";
+            Document document = Jsoup.connect(url).get();
+            Elements liveScoreElements = document.select("div.cb-mtch-lst.cb-tms-itm");
+            for (Element match : liveScoreElements) {
+                HashMap<String, String> liveMatchInfo = new LinkedHashMap<>();
+                String teamsHeading = match.select("h3.cb-lv-scr-mtch-hdr").select("a").text();
+                String matchNumberVenue = match.select("span").text();
+                Elements matchBatTeamInfo = match.select("div.cb-hmscg-bat-txt");
+                String battingTeam = matchBatTeamInfo.select("div.cb-hmscg-tm-nm").text();
+                String score = matchBatTeamInfo.select("div.cb-hmscg-tm-nm+div").text();
+                Elements bowlTeamInfo = match.select("div.cb-hmscg-bwl-txt");
+                String bowlTeam = bowlTeamInfo.select("div.cb-hmscg-tm-nm").text();
+                String bowlTeamScore = bowlTeamInfo.select("div.cb-hmscg-tm-nm+div").text();
+                String textLive = match.select("div.cb-text-live").text();
+                String textComplete = match.select("div.cb-text-complete").text();
+                //getting match link
+                String matchLink = match.select("a.cb-lv-scrs-well.cb-lv-scrs-well-live").attr("href").toString();
+
+                Match match1 = new Match();
+                match1.setTeamHeading(teamsHeading);
+                match1.setMatchNumberVenue(matchNumberVenue);
+                match1.setBattingTeam(battingTeam);
+                match1.setBattingTeamScore(score);
+                match1.setBowlingTeam(bowlTeam);
+                match1.setBowlingTeamScore(bowlTeamScore);
+                match1.setLiveText(textLive);
+                match1.setMatchLink(matchLink);
+                match1.setTextComplete(textComplete);
+                match1.setMatchStatus();
+
+                matches.add(match1);
+
+                updateMatchInDb(match1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return matches;
+    }
+
+    private void updateMatchInDb(Match match) {
+        Match existingMatch = matchRepository.findByTeamHeading(match.getTeamHeading()).orElse(null);
+        if (existingMatch == null) {
+            matchRepository.save(match);
+        } else {
+            match.setId(existingMatch.getId());
+            matchRepository.save(match);
+        }
     }
 
     @Override
