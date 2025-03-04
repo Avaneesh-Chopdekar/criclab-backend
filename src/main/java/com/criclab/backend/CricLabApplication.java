87d19590edbdc85ab8de01f805942c38e67c56c8
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-04 11:50:46 +0530

    initial commit

---
+++ src/main/java/com/criclab/backend/CricLabApplication.java
@@ -0,0 +1,13 @@
+package com.criclab.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/criclab/backend/CricLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.criclab.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
