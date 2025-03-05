605c3cad59e681b78e2d37ee6e730c998779c469
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-05 23:34:55 +0530

    added SwaggerConfig for configuring swagger ui api docs

---
+++ src/main/java/com/criclab/backend/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.criclab.backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPIConfig() {
+        return new OpenAPI().info(
+                        new Info()
+                                .title("CricLab Backend API")
+                                .description("A Real-time Cricket Score Application by Avaneesh Chopdekar")
+                                .version("1.0.0")
+                )
+                .servers(
+                        List.of(
+                                new Server().url("http://localhost:8080")
+                                        .description("Local Development Server"),
+                                new Server().url("https://criclab-backend.onrender.com")
+                                        .description("Production Server")
+                        )
+                ) // TODO: Change production server url after deployment
+                .tags(
+                        List.of(
+                                new Tag().name("Matches").description("Endpoints for managing matches")
+                        )
+                );
+    }
+}
--- src/main/java/com/criclab/backend/controller/MatchController.java
+++ src/main/java/com/criclab/backend/controller/MatchController.java
@@ -2,6 +2,7 @@ package com.criclab.backend.controller;
 
 import com.criclab.backend.entity.Match;
 import com.criclab.backend.services.MatchService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/matches")
 @CrossOrigin(origins = "http://localhost:4200")
+@Tag(name = "Matches", description = "Endpoints for managing matches")
 public class MatchController {
 
     private final MatchService matchService;
