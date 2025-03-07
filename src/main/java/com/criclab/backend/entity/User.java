44c5da2708be9a01c7a527be6fc1188ea9398121
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-07 12:21:18 +0530

    added validations to email and password field in User entity and added index for email field

--- src/main/java/com/criclab/backend/entity/User.java
+++ src/main/java/com/criclab/backend/entity/User.java
@@ -1,6 +1,8 @@
 package com.criclab.backend.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,14 +13,20 @@ import java.util.ArrayList;
 
 @Data
 @Entity
-@Table(name = "admin_user")
+@Table(name = "admin_user", indexes = {@Index(name = "idx_user_email", columnList = "email", unique = true)})
 public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Email
+    @Column(nullable = false, unique = true)
     private String email;
+
+    // Password must be at least 8 characters long, 30 characters maximum and contain at least one lowercase letter, one uppercase letter, one digit and one special character
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,30}$")
+    @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
