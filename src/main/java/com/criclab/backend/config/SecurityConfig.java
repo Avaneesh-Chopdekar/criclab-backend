c04621119222189b0e62d94dbb0a081d744b3fd5
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-07 12:43:42 +0530

    added CustomAuthenticationEntryPoint in SecurityConfig class

--- src/main/java/com/criclab/backend/config/SecurityConfig.java
+++ src/main/java/com/criclab/backend/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,6 +33,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(authenticationEntryPoint))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
