e6bd636830e68e0f86c8122d0c12e7a0008bb2de
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-07 11:24:46 +0530

    made User entity implement UserDetails interface

--- src/main/java/com/criclab/backend/entity/User.java
+++ src/main/java/com/criclab/backend/entity/User.java
@@ -2,14 +2,17 @@ package com.criclab.backend.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.ArrayList;
 
 @Data
 @Entity
 @Table(name = "admin_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +24,39 @@ public class User {
     @Column(nullable = false)
     private boolean superAdmin = false;
 
+    @Column(nullable = false)
+    private boolean enabled = true;
+
     @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<SystemLog> systemLogs = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
