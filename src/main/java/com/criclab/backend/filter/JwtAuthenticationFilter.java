461b523c64ec4227e582f3251c893f01b7b88a97
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-07 12:49:05 +0530

    moved filterChain.doFilter(request, response); outside of condition in doFilterInternal() method in JwtAuthenticationFilter class

--- src/main/java/com/criclab/backend/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/criclab/backend/filter/JwtAuthenticationFilter.java
@@ -37,8 +37,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                 }
             }
-
-            filterChain.doFilter(request, response);
         }
+
+        filterChain.doFilter(request, response);
     }
 }
